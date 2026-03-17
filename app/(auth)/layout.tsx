9202166f68db8cea135e6129790a60f62a264ecd
Angshuman Kashyap <angshumankashyap69yap69@gmail.com>
2026-03-17 08:07:14 +0530

    RESET THE PROJECT

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Auth = ({children}:{children: React.ReactNode}) => {
-  return (
-    <div className='flex justify-center items-center w-full h-screen'>{children}</div>
-  )
-}
-
-export default Auth
