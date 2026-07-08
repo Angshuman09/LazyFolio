c1214195eb468e119dd981794c99062fa1b2d281
Angshuman Kashyap <angshumankashyap69yap69@gmail.com>
2026-07-08 14:04:54 +0530

    update auth client to use NEXT_PUBLIC_SITE_URL for baseURL

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_SITE_URL as string: "http://localhost:3000",
+    baseURL: process.env.NEXT_PUBLIC_SITE_URL
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient;
